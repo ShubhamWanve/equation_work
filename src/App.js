bc9e9af1c4578c6095f104f0b2d1825c2a8a73eb
ShubhamWanve <86403359+ShubhamWanve@users.noreply.github.com>
2021-12-07 15:14:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const key="9367bee3da6d2649df30aea0fe04ec3b";
-const pass="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";
-const base64encodedData = Buffer.from(key + ':' + pass).toString('base64');
-const headers={
-  "Content-Type": "application/json",
-  "Accept": "application/json", 
-  "Authorization": 'Basic ' + base64encodedData,
-  "Access-Control-Allow-Origin": "https://santoshi12.myshopify.com/admin/api/2021-10/products.json",
-  'Access-Control-Allow-Credentials':true
-}
-
 export default class App extends Component {
-
-  componentDidMount() {
-    axios.get(`https://santoshi12.myshopify.com/admin/api/2021-10/products.json`,{ method:'GET',headers})
-    .then(res => {
-      console.log("Shubham:-",res)
-      // this.setState({ persons });
-    })
-}
   render() {
     return (
       <div>

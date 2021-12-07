48878f960d765cc2f759d5ef75240d8381067e47
ShubhamWanve <86403359+ShubhamWanve@users.noreply.github.com>
2021-12-07 15:13:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const key="9367bee3da6d2649df30aea0fe04ec3b";
 const pass="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";
 const base64encodedData = Buffer.from(key + ':' + pass).toString('base64');
 const headers={
-  "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+  "Content-Type": "application/json",
   "Accept": "application/json", 
   "Authorization": 'Basic ' + base64encodedData,
   "Access-Control-Allow-Origin": "https://santoshi12.myshopify.com/admin/api/2021-10/products.json",
